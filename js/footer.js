437bdf0e0c8b8fab08b30e096890f2425ea020e0
Tobi V <hialmberi95@yahoo.com>
2022-09-24 17:03:06 -0500

    Create site

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+
+document.querySelector(".writeFooter").innerHTML = `
+    <footer>
+        <p>T. Valentine &copy; 2022</p>
+    </footer>
+`;
---
+++ js/header.js
@@ -0,0 +1,16 @@
+document.querySelector(".writeHeader").innerHTML = `
+    <header>
+        <div class="nav">
+            <a href="/comic-home.html">Comics</a>
+            <a href="/index.html">Illustrations</a>
+            <a href="/about.html">About</a>
+        </div>
+        <div class="header">
+            <h1> Tobi Valentine </h1>
+        </div>
+    </header>
+`;
+/*
+<a href="https://h0neymice.neocities.org/comic-home.html">Comics</a>
+<a href="https://h0neymice.neocities.org/index.html">Illustrations</a>
+<a href="https://h0neymice.neocities.org/about.html">About</a>*/
