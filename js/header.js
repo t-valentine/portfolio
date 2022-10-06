ffd24f4dcabd3084f7aa96b36b6861cb282f1a43
Tobi V <hialmberi95@yahoo.com>
2022-10-06 18:35:08 -0500

    Update header links

--- js/header.js
+++ js/header.js
@@ -1,11 +1,11 @@
 document.querySelector(".writeHeader").innerHTML = `
     <header>
         <div class="nav">
-            <a href="/comic-home.html">Comics</a>
+            <a href="https://h0neymice.neocities.org/comic-home.html">Comics</a>
             <p>&#128123;</p>
-            <a href="/index.html">Illustrations</a>
+            <a href="https://h0neymice.neocities.org/index.html">Illustrations</a>
             <p>&#128123;</p>
-            <a href="/about.html">About</a>
+            <a href="https://h0neymice.neocities.org/about.html">About</a>
         </div>
         <div class="header">
             <h1> &#127875; Tobi Valentine &#127875; </h1>
