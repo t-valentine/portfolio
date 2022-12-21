dd170c401dfdfb29cebfd16b6fb7263ca10535ed
Tobi Wallace <twallace@catalyte.io>
2022-12-21 11:59:09 -0600

    Create rudimentary modal

--- public/js/header.js
+++ public/js/header.js
@@ -10,25 +10,31 @@ console.log(relativePath)
 
 let relativePath = ".";
 let currentURL = document.URL;
-if ( currentURL.includes("comics/") ) {
+if (currentURL.includes("comics/")) {
   relativePath = "../..";
 }
-console.log(relativePath)
 
-
-document.querySelector(".writeHeader").innerHTML = `
+document.querySelector(".writeHeader").innerHTML =
+  `
     <header>
         <div class="header">
-        <h1> <a href="`+ relativePath +`/index.html" class="nameHeader"> &#10052 Tobi Valentine &#10052 </a> </h1>
+        <h1> <a href="` +
+  relativePath +
+  `/index.html" class="nameHeader"> &#10052 Tobi Valentine &#10052 </a> </h1>
         </div>
         <div class="nav">
-          <a href="`+ relativePath +`/comic-home.html" id="comics" class="unselected-page">Comics</a>
-          <a href="`+ relativePath +`/index.html" id="home" class="unselected-page">Illustrations</a>
-          <a href="`+ relativePath +`/about.html" id="about" class="unselected-page">About</a> 
+          <a href="` +
+  relativePath +
+  `/comic-home.html" id="comics" class="unselected-page">Comics</a>
+          <a href="` +
+  relativePath +
+  `/index.html" id="home" class="unselected-page">Illustrations</a>
+          <a href="` +
+  relativePath +
+  `/about.html" id="about" class="unselected-page">About</a> 
         </div>
     </header>
 `;
-
 
 if (currentURL.includes("comic-home") || currentURL.includes("comics")) {
   document.getElementById("comics").classList.remove("unselected-page");
@@ -36,9 +42,7 @@ if (currentURL.includes("comic-home") || currentURL.includes("comics")) {
 } else if (currentURL.includes("about")) {
   document.getElementById("about").classList.remove("unselected-page");
   document.getElementById("about").classList.add("selected-page");
-}
-else {
+} else {
   document.getElementById("home").classList.remove("unselected-page");
   document.getElementById("home").classList.add("selected-page");
 }
-
