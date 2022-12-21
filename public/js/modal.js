c73dd4e67da9cd9785e3fbaf6d718e3689a135ac
Tobi Wallace <twallace@catalyte.io>
2022-12-21 14:57:49 -0600

    reacts in live time to window resize

--- public/js/modal.js
+++ public/js/modal.js
@@ -43,20 +43,34 @@ function changePic(e) {
     imgTitle.innerText = e.target.title;
     imgDesc.innerText = e.target.dataset.desc;
     modal.style.top = window.scrollY + 20 + "px";
-    if (window.innerWidth < 950) {
-      modal.style.maxHeight = window.innerWidth - 50 + "px";
-      modal.style.maxWidth = window.innerWidth - 50 + "px";
-      featImg.style.maxHeight = window.innerWidth - 150 + "px";
-      featImg.style.maxWidth = window.innerWidth - 150 + "px";
-      if (window.innerWidth < 600) {
-        modal.style.maxHeight = window.innerWidth - 10 + "px";
-        modal.style.maxWidth = window.innerWidth - 10 + "px";
-        featImg.style.maxHeight = window.innerWidth - 20 + "px";
-        featImg.style.maxWidth = window.innerWidth - 20 + "px";
-      }
-    }
   }
 }
+
+let updateModalSize = () => {
+  if (window.innerWidth < 950 && window.innerWidth >= 600) {
+    modal.style.maxHeight = window.innerWidth - 50 + "px";
+    modal.style.maxWidth = window.innerWidth - 50 + "px";
+    modal.style.padding = "20px";
+    featImg.style.maxHeight = window.innerWidth - 150 + "px";
+    featImg.style.maxWidth = window.innerWidth - 150 + "px";
+  } else if (window.innerWidth < 600) {
+    let height = window.innerWidth;
+    let width = window.innerWidth - 100;
+    modal.style.maxHeight = height + "px";
+    modal.style.width = width + "px";
+    featImg.style.maxHeight = height - 20 + "px";
+    featImg.style.maxWidth = width - 20 + "px";
+  } else {
+    modal.style.maxHeight = null;
+    modal.style.maxWidth = null;
+    modal.style.padding = null;
+    featImg.style.maxHeight = null;
+    featImg.style.maxWidth = null;
+  }
+};
+
+window.addEventListener("load", updateModalSize);
+window.onresize = updateModalSize;
 
 /*
 
