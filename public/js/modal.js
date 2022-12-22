3508ca12a95be9c09a958f8d7fe98c12fd9d9a47
Tobi V <tobiasvalentine95@gmail.com>
2022-12-22 16:57:59 -0600

    Remove blue box and have only reactive sizing

--- public/js/modal.js
+++ public/js/modal.js
@@ -48,26 +48,17 @@ function changePic(e) {
 
 // updates modal and image size
 let updateModalSize = () => {
-  if (window.innerWidth < 950 && window.innerWidth >= 600) {
-    modal.style.maxHeight = window.innerWidth - 50 + "px";
-    modal.style.maxWidth = window.innerWidth - 50 + "px";
-    modal.style.padding = "20px";
-    featImg.style.maxHeight = window.innerWidth - 150 + "px";
-    featImg.style.maxWidth = window.innerWidth - 150 + "px";
-  } else if (window.innerWidth < 600) {
-    let height = window.innerWidth;
-    let width = window.innerWidth - 100;
-    modal.style.maxHeight = height + "px";
-    modal.style.width = width + "px";
-    featImg.style.maxHeight = height - 20 + "px";
-    featImg.style.maxWidth = width - 20 + "px";
-  } else {
-    modal.style.maxHeight = null;
-    modal.style.maxWidth = null;
-    modal.style.padding = null;
-    featImg.style.maxHeight = null;
-    featImg.style.maxWidth = null;
+  let windowWidth = window.innerWidth;
+  let windowHeight = window.innerHeight;
+  let padding = 20
+  if (windowWidth <= 900) {
+    modal.style.padding = "15px";
+    padding = 15;
   }
+
+  featImg.style.maxHeight = windowHeight - 100 + "px";
+  featImg.style.maxWidth = windowWidth - 100 + "px";
+
 };
 
 window.addEventListener("load", updateModalSize);
