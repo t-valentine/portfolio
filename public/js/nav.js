f7fa72fbb9617984145bcb4248b32ac38f8d1575
Tobi V <tobiasvalentine95@gmail.com>
2023-05-10 23:39:23 -0500

    more styling

--- public/js/nav.js
+++ public/js/nav.js
@@ -5,75 +5,20 @@ if (currentURL.includes("comics/")) {
 }
 
 document.querySelector("#mainNav").innerHTML = `
-<div class="nav-header">
-  <div id="mobile-format">
-  <a href="` +
+<a href="` +
   relativePath +
   `/index.html">    <img src="` +
-  relativePath + `/img/logo.png" id="header-image"></a>
-  <div class="desktop-menu">
-      <a href="` +
-      relativePath +
-      `/index.html">Illustrations</a>
-      <a href="` +
-      relativePath +
-      `/comics.html">Comics</a></li>
-      <a href="` +
-      relativePath +
-      `/extras.html">Miscellaneous</a></li>
-      <a href="` +
-      relativePath +
-      `/about.html">About</a>
-      
-    </div>
-    <div id="menu-button">
-      Menu
-    </div>
-  </div>
-  <div id="mobile-menu">
-    <a href="` +
-    relativePath +
-    `/index.html">Illustrations</a>
-    <a href="` +
-    relativePath +
-    `/comics.html">Comics</a>
-    <a href="` +
-    relativePath +
-    `/extras.html">Miscellaneous</a>
-    <a href="` +
-    relativePath +
-    `/about.html">About</a>
-  </div>
-</div>
+  relativePath + `/img/logo.png" id="header-image">
+<a href="` +
+relativePath +
+`/index.html" class="nav-links">Illustrations</a>
+<a href="` +
+relativePath +
+`/comics.html" class="nav-links">Comics</a></li>
+<a href="` +
+relativePath +
+`/extras.html" class="nav-links">Other Projects</a></li>
+<a href="` +
+relativePath +
+`/about.html" class="nav-links">About</a>
 `;
-
-let menuButton = document.getElementById("menu-button");
-
-menuButton.addEventListener("click", displayMenu);
-
-function displayMenu() {
-  var x = document.getElementById("mobile-menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
-/*
-This would go under regular header
-
-<div class="social_media">
-      <a href="" target="_blank">
-          <i class="fa-brands fa-twitter"></i>
-          <p>Twitter</p>
-      </a>
-      <a href="" target="_blank">
-          <i class="fa-brands fa-tumblr"></i>
-          <p>Tumblr</p>
-      </a>
-      <a href="" target="_blank">
-          <i class="fa-solid fa-bug"></i>
-          <p>Co-Host</p>
-      </a>
-  </div> */
