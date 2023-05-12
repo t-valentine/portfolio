664a30370f3e7d79d2aceef1b3f86400458a16f6
Tobi V <tobiasvalentine95@gmail.com>
2023-05-11 21:00:41 -0500

    worst commit messages of all time

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,26 +1,28 @@
 let relativePath = ".";
 let currentURL = document.URL;
 if (currentURL.includes("comics/")) {
-  relativePath = "../..";
+	relativePath = "../..";
 }
 
-document.querySelector("#mainNav").innerHTML = `
+document.querySelector("#mainNav").innerHTML =
+	`
 <a href="` +
-  relativePath +
-  `/index.html">    <img src="` +
-  relativePath + `/img/logo.png" id="header-image">
+	relativePath +
+	`/index.html">    <img src="` +
+	relativePath +
+	`/img/logo.png" id="header-image">
   <div id="secondary-nav">
 <a href="` +
-relativePath +
-`/index.html" class="nav-links">Illustrations</a>
+	relativePath +
+	`/index.html" class="nav-links">Illustrations</a>
 <a href="` +
-relativePath +
-`/comics.html" class="nav-links">Comics</a></li>
+	relativePath +
+	`/comics.html" class="nav-links">Comics</a></li>
 <a href="` +
-relativePath +
-`/extras.html" class="nav-links">Other Projects</a></li>
+	relativePath +
+	`/projects.html" class="nav-links">Other Projects</a></li>
 <a href="` +
-relativePath +
-`/about.html" class="nav-links">About</a>
+	relativePath +
+	`/about.html" class="nav-links">About</a>
 </div>
 `;
